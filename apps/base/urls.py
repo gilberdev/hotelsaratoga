e3f7989392f7fabb3154fa152d469948f735a4ab
unknown <g.garcia@lab.matcom.uh.cu>
2015-12-09 00:08:54 -0500

    Agregando nuevas paginas y trabajando en hoteles
    Los disennos de estas paginas estan incompletos

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -5,4 +5,7 @@ from django.conf.urls import url, patterns
 
 urlpatterns = patterns('base.views',
     url(r'^$', 'home', name='home'),
+    url(r'^history$', 'history', name='history'),
+    url(r'^news$', 'news', name='news'),
+    url(r'^hotels$', 'hotels', name='hotels'),
 )
--- apps/base/views.py
+++ apps/base/views.py
@@ -6,3 +6,17 @@ from django.shortcuts import render
 def home(request):
     """ Default view for the root """
     return render(request, 'base/home.html')
+
+def history(request):
+    """ Default view for the root """
+    return render(request, 'base/history.html')
+
+def news(request):
+    """ Default view for the root """
+    return render(request, 'base/news.html')
+
+def hotels(request):
+    """ Default view for the root """
+    return render(request, 'base/hotels.html')
+
+
