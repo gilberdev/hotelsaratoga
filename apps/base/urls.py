632f7ed983c093d268d1633ef410a78b62b0bfca
Gilberto Garcia <g.garcia9390@gmail.com>
2016-01-14 12:03:32 -0500

    Pasando parametros en la ruta de noticias

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls import url, patterns
 urlpatterns = patterns('base.views',
     url(r'^$', 'home', name='home'),
     url(r'^history$', 'history', name='history'),
-    url(r'^news$', 'news', name='news'),
+    url(r'^news/', 'news', name='news'),
+    url(r'^news(?P<new_id>\d*)$', 'news', name='news'),
     url(r'^hotels$', 'hotels', name='hotels'),
 )
